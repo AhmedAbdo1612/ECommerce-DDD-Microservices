942eb2aaa58d79ff49d879dd5d7e900d63a82dc5
ahmed abdo <ahmaadaabdo@gmail.com>
2026-07-14 09:39:31 +0300

    Update project files, clean git index from obj/.vs, and add WebApps directory

---
+++ WebApps/Shopping.Web/Models/Ordering/PaginatedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Shopping.Web.Models.Ordering;
+
+public class PaginatedResult<T>(int pageIndex, int pageSize, long count, IEnumerable<T> data) where T : class
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pageSize;
+    public long Count { get; } = count;
+    public IEnumerable<T> Data { get; } = data;
+}
---
+++ WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Web.Models.Catalog;
+
+namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        Task<GetProductResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+        Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
+        Task<GetProductByIdResponse> GetProductById(Guid id);
+    }
+}
